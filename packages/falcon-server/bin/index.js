1818a6acc6214015f5c91b4c86bb7c16b6ff97a9
Mateusz Wozniak <kontakt@mateuszwozniak.pl>
2018-08-09 16:00:27 +0200

    Basic setup falcon-server and shop-with-blog/server

---
+++ packages/falcon-server/bin/index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const FalconServer = require('../src/index.js');
+
+// get passed file name and create full path to that file
+const createConfigFileName = process.argv[2];
+const createConfigFilePath = path.join(process.cwd(), createConfigFileName);
+
+// just make sure that file exists before using it
+if (!fs.existsSync(createConfigFilePath)) {
+  console.error(`ERROR: Passed config file: ${createConfigFileName} does not exist! Aborting.`);
+  process.exit(1);
+}
+
+if (!fs.statSync(createConfigFilePath).isFile()) {
+  console.error(`ERROR: Passed argument: ${createConfigFileName} is not a file! Aborting.`);
+  process.exit(1);
+}
+
+let config = {};
+try {
+  const createConfig = require(createConfigFilePath); // eslint-disable-line import/no-dynamic-require
+  config = typeof createConfig === 'function' ? createConfig() : createConfig;
+} catch (ex) {
+  console.error('ERROR: Cannot load configuration file. Details:');
+  console.log(ex.stack);
+  process.exit(1);
+}
+
+const server = new FalconServer(config);
+server.start();
--- packages/falcon-server/src/index.js
+++ packages/falcon-server/src/index.js
@@ -1,34 +1,43 @@
 const Koa = require('koa');
 const { ApolloServer, gql } = require('apollo-server-koa');
 
-const PORT = Number(process.env.PORT) || 4000;
 const ENV = process.env.NODE_ENV || 'development';
 const isDevelopment = ENV === 'development';
 
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    hello: String
+class FalconServer {
+  constructor(conf) {
+    this.config = conf;
   }
-`;
 
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => 'World'
+  start() {
+    // Construct a schema, using GraphQL schema language
+    const typeDefs = gql`
+      type Query {
+        hello: String
+      }
+    `;
+
+    // Provide resolver functions for your schema fields
+    const resolvers = {
+      Query: {
+        hello: () => 'World'
+      }
+    };
+
+    const server = new ApolloServer({
+      tracing: isDevelopment,
+      playground: isDevelopment,
+      typeDefs,
+      resolvers
+    });
+
+    const app = new Koa();
+    server.applyMiddleware({ app });
+
+    app.listen({ port: this.config.port }, () => {
+      console.log(`🚀 Server ready at http://localhost:${this.config.port}`);
+    });
   }
-};
+}
 
-const server = new ApolloServer({
-  tracing: isDevelopment,
-  playground: isDevelopment,
-  typeDefs,
-  resolvers
-});
-
-const app = new Koa();
-server.applyMiddleware({ app });
-
-app.listen({ port: PORT }, () => {
-  console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
-});
+module.exports = FalconServer;
