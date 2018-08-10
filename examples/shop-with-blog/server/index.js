a17f599158c48a4e785b1898dad6c79cf757b850
Mateusz Wozniak <kontakt@mateuszwozniak.pl>
2018-08-10 17:24:12 +0200

    Start server debugger in default start command

--- examples/shop-with-blog/server/index.js
+++ examples/shop-with-blog/server/index.js
@@ -1,3 +1,8 @@
-module.exports = {
+global.__SERVER__ = true; // eslint-disable-line no-underscore-dangle
+
+const FalconServer = require('@deity/falcon-server');
+
+const server = new FalconServer({
   port: 4000
-};
+});
+server.start();
