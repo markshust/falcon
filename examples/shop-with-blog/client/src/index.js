f33865a58c54956de6ae4e4703ba5818d127ceed
tutok <walas.marcin@gmail.com>
2018-08-10 11:41:55 +0200

    added index.js into example - entry point for entire application

--- examples/shop-with-blog/client/src/index.js
+++ examples/shop-with-blog/client/src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Route from 'react-router-dom/Route';
-import Switch from 'react-router-dom/Switch';
-import Home from './Home';
-import './App.css';
+import App from './App';
 
-const App = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-  </Switch>
-);
-
-export default App;
+export default {
+  component: <App />
+  // onServerCreated: server => { console.log('created'); },
+  // onServerInitialized: server => { console.log('initialized'); },
+  // onServerStarted: server => { console.log('started'); }
+};
