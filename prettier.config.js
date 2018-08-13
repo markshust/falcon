d42b81314ad634803a09beffc1bdb09f01428e4f
tutok <walas.marcin@gmail.com>
2018-08-13 18:18:55 +0200

    fixed issue with uknown token ...

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,11 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const esLint = require('@deity/eslint-config-falcon');
 
-module.exports = {
-  ...esLint.rules['prettier/prettier'][1],
-
+module.exports = Object.assign({}, esLint.rules['prettier/prettier'][1], {
   overrides: [
     {
       files: ['.eslintrc'],
       options: { parser: 'json' }
     }
   ]
-};
+});
