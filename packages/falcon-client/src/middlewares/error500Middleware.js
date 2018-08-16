1bcd7d95c68ffe2b8b13c15a8bf712e6c69f422f
tutok <walas.marcin@gmail.com>
2018-08-16 18:04:31 +0200

    added middleware which serve custom error page for 500 from falcon client or example if  overridden

---
+++ packages/falcon-client/src/middlewares/error500Middleware.js
@@ -0,0 +1,20 @@
+import path from 'path';
+import fs from 'fs';
+import Logger from '@deity/falcon-logger';
+import send from 'koa-send';
+import paths from '@hostSrc/clientApp/paths';
+
+export default async (ctx, next) => {
+  try {
+    await next();
+  } catch (error) {
+    Logger.error(`Error: ${error}`);
+
+    const root = fs.existsSync(path.join(paths.razzle.appSrc, `views/errors/500.html`))
+      ? path.join(paths.razzle.appSrc, `views/errors`)
+      : path.join(paths.falconClient.appSrc, `views/errors`);
+
+    ctx.status = 500;
+    await send(ctx, '/500.html', { root });
+  }
+};
