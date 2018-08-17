82ea77878a5cc45f0eca1a00332a0fe320cd5435
tutok <walas.marcin@gmail.com>
2018-08-17 10:37:15 +0200

    if serverSideRendering is false then ssrMiddleware will not be registered

--- packages/falcon-client/src/server.js
+++ packages/falcon-client/src/server.js
@@ -1,4 +1,3 @@
-import fetch from 'isomorphic-unfetch';
 import Koa from 'koa';
 import serve from 'koa-static';
 import helmet from 'koa-helmet';
@@ -9,19 +8,18 @@ import ssr from './middlewares/ssrMiddleware';
 import htmlShellRenderer from './middlewares/htmlShellRendererMiddleware';
 import error500 from './middlewares/error500Middleware';
 
-Logger.setLogLevel(ClientApp.config.logLevel);
+const { config } = ClientApp;
+Logger.setLogLevel(config.logLevel);
 
 // eslint-disable-next-line
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
-// Polyfill fetch() on the server (used by apollo-client)
-global.fetch = fetch;
-
-// Initialize `koa-router` and setup a route listening on `GET /*`
-// Logic has been splitted into two chained middleware functions
-// @see https://github.com/alexmingoia/koa-router#multiple-middleware
 const router = new Router();
-router.get('/*', ssr, htmlShellRenderer);
+if (config.serverSideRendering) {
+  router.get('/*', ssr, htmlShellRenderer);
+} else {
+  router.get('/*', htmlShellRenderer);
+}
 
 // Intialize and configure Koa application
 const server = new Koa();
