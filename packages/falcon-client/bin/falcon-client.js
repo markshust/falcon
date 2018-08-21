3b1a610d32e94d1973a5e712d463a2784dbe9b5d
tutok <walas.marcin@gmail.com>
2018-08-21 12:43:45 +0200

    added cleaing build dir when `start` dev server, also used extracted ealier razzle() function

--- packages/falcon-client/bin/falcon-client.js
+++ packages/falcon-client/bin/falcon-client.js
@@ -1,32 +1,20 @@
 #!/usr/bin/env node
-const spawn = require('cross-spawn');
+const fs = require('fs-extra');
+const razzle = require('./../src/razzle');
+const paths = require('./../src/razzle/paths');
 
 const script = process.argv[2];
 const args = process.argv.slice(3);
 
 switch (script) {
+  case 'start': {
+    fs.emptyDirSync(paths.razzle.appBuild);
+    razzle(script, args);
+    break;
+  }
   case 'build':
-  case 'start':
   case 'test': {
-    const result = spawn.sync('node', [require.resolve(`razzle/scripts/${script}`)].concat(args), { stdio: 'inherit' });
-
-    if (result.signal) {
-      if (result.signal === 'SIGKILL') {
-        console.log(
-          'The build failed because the process exited too early. ' +
-            'This probably means the system ran out of memory or someone called ' +
-            '`kill -9` on the process.'
-        );
-      } else if (result.signal === 'SIGTERM') {
-        console.log(
-          'The build failed because the process exited too early. ' +
-            'Someone might have called `kill` or `killall`, or the system could ' +
-            'be shutting down.'
-        );
-      }
-      process.exit(1);
-    }
-    process.exit(result.status);
+    razzle(script, args);
     break;
   }
   default:
