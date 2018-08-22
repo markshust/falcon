075fa6b28e5c98c69f16d11a4602eb7f0b97c890
tutok <walas.marcin@gmail.com>
2018-08-22 15:51:45 +0200

    make production bundle error reproducable

--- packages/falcon-client/src/middlewares/error500Middleware.js
+++ packages/falcon-client/src/middlewares/error500Middleware.js
@@ -2,7 +2,7 @@ import path from 'path';
 import fs from 'fs';
 import Logger from '@deity/falcon-logger';
 import send from 'koa-send';
-// import paths from '@hostSrc/razzle/paths';
+import paths from '@hostSrc/razzle/paths';
 
 /**
  * Custom 500 error middleware.
@@ -16,27 +16,27 @@ export default async (ctx, next) => {
   } catch (error) {
     Logger.error(`Error: ${error}`);
 
-    path.resolve(path.join(__dirname, '/../views/errors/500.html'));
-
-    const rootDir =
-      process.env.NODE_ENV === 'production'
-        ? path.resolve(path.join(process.env.RAZZLE_PUBLIC_DIR, './../../views/errors'))
-        : path.resolve(path.join(process.env.RAZZLE_PUBLIC_DIR, './../views/errors'));
+    // path.resolve(path.join(__dirname, '/../views/errors/500.html'));
 
     // TODO way of resolving paths (@hostSrc/razzle/paths) needs to be fixed!
-    // const root = fs.existsSync(path.join(paths.razzle.appSrc, `views/errors/500.html`))
-    //   ? path.join(paths.razzle.appSrc, `views/errors`)
-    //   : path.join(paths.falconClient.appSrc, `views/errors`);
-
-    // ctx.status = 500;
-    // await send(ctx, '/500.html', { root });
+    const root = fs.existsSync(path.join(paths.razzle.appSrc, `views/errors/500.html`))
+      ? path.join(paths.razzle.appSrc, `views/errors`)
+      : path.join(paths.falconClient.appSrc, `views/errors`);
 
     ctx.status = 500;
+    await send(ctx, '/500.html', { root });
 
-    if (fs.existsSync(path.join(rootDir, `500.html`))) {
-      await send(ctx, '/500.html', { root });
-    } else {
-      ctx.body = 'Internal Server Error';
-    }
+    // const rootDir =
+    //   process.env.NODE_ENV === 'production'
+    //     ? path.resolve(path.join(process.env.RAZZLE_PUBLIC_DIR, './../../views/errors'))
+    //     : path.resolve(path.join(process.env.RAZZLE_PUBLIC_DIR, './../views/errors'));
+
+    // ctx.status = 500;
+
+    // if (fs.existsSync(path.join(rootDir, `500.html`))) {
+    //   await send(ctx, '/500.html', { root });
+    // } else {
+    //   ctx.body = 'Internal Server Error';
+    // }
   }
 };
