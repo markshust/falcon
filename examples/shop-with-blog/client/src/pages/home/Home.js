b474f971970870042d7bc4bfbabb722cf39d84dd
tutok <walas.marcin@gmail.com>
2018-08-23 11:04:14 +0200

    used react i18 next on home view

--- examples/shop-with-blog/client/src/pages/home/Home.js
+++ examples/shop-with-blog/client/src/pages/home/Home.js
@@ -1,53 +1,63 @@
 import React from 'react';
+import { translate } from 'react-i18next';
+import PropTypes from 'prop-types';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import { Link } from 'react-router-dom';
 import logo from 'public/logo.png';
 import './Home.css';
 
-const Home = () => (
-  <div className="Home">
-    <div className="Home-header">
-      <img src={logo} className="Home-logo" alt="logo" />
-      <h2>Welcome to @deity + Razzle + Koa </h2>
-    </div>
-    <pre className="Home-intro">
-      To get started, edit <b>src/App.js</b> or <b>src/Home.js</b> and save to reload.
-    </pre>
-    <Query
-      query={gql`
-        query Hi {
-          hi @client
-        }
-      `}
-    >
-      {({ data: { hi } }) => (
-        <pre>
-          Saying <b>{hi}</b> from the Client State!
-        </pre>
-      )}
-    </Query>
-    <ul className="Home-resources">
-      <li>
-        <a href="https://github.com/jaredpalmer/razzle">Docs</a>
-      </li>
-      <li>
-        <Link to="/post">Post (dynamic)</Link>
-      </li>
-      <li>
-        <Link to="/shop">Shop (dynamic)</Link>
-      </li>
-      <li>
-        <a href="http://koajs.com">Koa official site</a>
-      </li>
-      <li>
-        <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
-      </li>
-      <li>
-        <a href="https://palmer.chat">Community Slack</a>
-      </li>
-    </ul>
-  </div>
-);
+const Home = props => {
+  const { t } = props;
 
-export default Home;
+  return (
+    <div className="Home">
+      <div className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
+        <h2>{t('welcome')}</h2>
+        <h2>Welcome to @deity + Razzle + Koa </h2>
+      </div>
+      <pre className="Home-intro">
+        To get started, edit <b>src/App.js</b> or <b>src/Home.js</b> and save to reload.
+      </pre>
+      <Query
+        query={gql`
+          query Hi {
+            hi @client
+          }
+        `}
+      >
+        {({ data: { hi } }) => (
+          <pre>
+            Saying <b>{hi}</b> from the Client State!
+          </pre>
+        )}
+      </Query>
+      <ul className="Home-resources">
+        <li>
+          <a href="https://github.com/jaredpalmer/razzle">Docs</a>
+        </li>
+        <li>
+          <Link to="/post">Post (dynamic)</Link>
+        </li>
+        <li>
+          <Link to="/shop">Shop (dynamic)</Link>
+        </li>
+        <li>
+          <a href="http://koajs.com">Koa official site</a>
+        </li>
+        <li>
+          <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
+        </li>
+        <li>
+          <a href="https://palmer.chat">Community Slack</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+Home.propTypes = {
+  t: PropTypes.func
+};
+
+export default translate()(Home);
---
+++ packages/falcon-client/src/i18n/i18nextClient.js
@@ -0,0 +1,25 @@
+import i18next from 'i18next';
+import XHR from 'i18next-xhr-backend';
+
+const defaultOptions = {
+  whitelist: ['en'], // base on extensions configuration
+  lng: 'en',
+  fallbackLng: 'en',
+  ns: ['common'],
+  defaultNS: 'common',
+  fallbackNS: 'common',
+  debug: true,
+  interpolation: {
+    escapeValue: false // not needed for react!!
+  }
+};
+
+export default options =>
+  i18next.use(XHR).init({
+    ...defaultOptions,
+    ...options,
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+      parse: data => data // WARNING this has to stay
+    }
+  });
