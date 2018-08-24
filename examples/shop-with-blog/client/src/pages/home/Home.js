ca45b9569077cb5b5eec8c8977453f6704351802
tutok <walas.marcin@gmail.com>
2018-08-24 10:03:44 +0200

    static files can not be server from public/static it needs to be public

--- examples/shop-with-blog/client/src/pages/home/Home.js
+++ examples/shop-with-blog/client/src/pages/home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import { Link } from 'react-router-dom';
-import logo from 'public/static/logo.png';
+import logo from 'public/logo.png';
 import './Home.css';
 
 const Home = () => (
