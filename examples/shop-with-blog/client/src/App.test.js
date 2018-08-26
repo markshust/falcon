5e63fb0b028c639476526d3a8f9b09ffa80664b3
Oleksandr Bordun <bordun.alexandr@gmail.com>
2018-08-26 22:14:51 +0200

    Adding initial test file to Falcon-client Fixing test in examples/shop-with-blog sub-project

--- examples/shop-with-blog/client/src/App.test.js
+++ examples/shop-with-blog/client/src/App.test.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import gql from 'graphql-tag';
+import { MockedProvider } from 'react-apollo/test-utils';
 import MemoryRouter from 'react-router-dom/MemoryRouter';
 import App from './App';
 
 describe('<App />', () => {
   test('renders without exploding', () => {
+    const mocks = [
+      {
+        request: {
+          query: gql`
+            query Hi {
+              hi @client
+            }
+          `
+        },
+        result: {
+          data: { hi: 'Hello world!' }
+        }
+      }
+    ];
     const div = document.createElement('div');
     ReactDOM.render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      </MockedProvider>,
       div
     );
   });
---
+++ packages/falcon-client/src/index.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MockedProvider } from 'react-apollo/test-utils';
+import MemoryRouter from 'react-router-dom/MemoryRouter';
+import Switch from 'react-router-dom/Switch';
+import wait from 'waait';
+import DynamicRoute from './components/DynamicRoute';
+import { GET_URL } from './graphql/url.gql';
+
+describe('falcon-client', () => {
+  it('Should render DynamicRoute content', async () => {
+    const mocks = [
+      {
+        request: {
+          query: GET_URL,
+          variables: { url: '/test' }
+        },
+        result: {
+          data: {
+            getUrl: { type: 'foo', url: '/test' }
+          }
+        }
+      }
+    ];
+
+    const Foo = () => <p>Bar</p>;
+    const components = { foo: Foo };
+
+    const App = renderer.create(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <MemoryRouter initialEntries={['/test']}>
+          <Switch>
+            <DynamicRoute components={components} />
+          </Switch>
+        </MemoryRouter>
+      </MockedProvider>
+    );
+
+    const tree = App.toJSON();
+    expect(tree.children).toContain('Loading...');
+    await wait(0);
+
+    const p = App.root.findByType('p');
+    expect(p.children).toContain('Bar');
+  });
+});
