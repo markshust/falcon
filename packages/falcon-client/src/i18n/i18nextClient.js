1e7905c92a06fffc9bef95ca6b09946b6fb58cf3
tutok <walas.marcin@gmail.com>
2018-08-27 22:18:25 +0200

    instead of loading json files, will dynamicly requre them

--- packages/falcon-client/src/i18n/i18nextClient.js
+++ packages/falcon-client/src/i18n/i18nextClient.js
@@ -10,15 +10,31 @@ const defaultOptions = {
   fallbackNS: 'common',
   interpolation: {
     escapeValue: false // not needed for react!!
+  },
+  react: {
+    wait: true
   }
 };
+
+const loadLocale = (url, options, callback) =>
+  import(/* webpackChunkName: "locales/[request]" */ `app-path/public/locales/${url}`)
+    .then(x => callback(x.default, { status: '200' }))
+    .catch(e => {
+      if (e.code === 'MODULE_NOT_FOUND') {
+        console.warn(`Can not load locale '${url}'! Will try to load fallback 'lag/ns.json'`);
+      } else {
+        console.warn(`Unexpected Error while loading locale '${url}'!\n${e}`);
+      }
+      callback(null, { status: '404' });
+    });
 
 export default (options = {}) =>
   i18next.use(XHR).init({
     ...defaultOptions,
     ...options,
     backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
-      parse: data => JSON.parse(data)
+      loadPath: '{{lng}}/{{ns}}.json',
+      parse: x => x,
+      ajax: loadLocale
     }
   });
