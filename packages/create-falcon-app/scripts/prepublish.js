71d6d358fa17be0b89e2f340362d1c92ed71d819
Oleksandr Bordun <bordun.alexandr@gmail.com>
2018-08-30 18:31:54 +0200

    Fixing examples path for create-falcon-app

--- packages/create-falcon-app/scripts/prepublish.js
+++ packages/create-falcon-app/scripts/prepublish.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs-extra');
 const { examplesPath } = require('../src');
 
-const examplesSourcePath = path.resolve('../../examples');
+const examplesSourcePath = path.resolve(__dirname, '../../../examples');
 
 fs.emptyDirSync(examplesPath);
 fs.copySync(examplesSourcePath, examplesPath);
