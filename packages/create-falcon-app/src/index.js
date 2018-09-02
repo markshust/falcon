75615755f928483c38989c4a37baa6b47ef77ed9
Oleksandr Bordun <bordun.alexandr@gmail.com>
2018-09-02 23:01:54 +0200

    Installing dependencies for newly generated falcon app

--- packages/create-falcon-app/src/index.js
+++ packages/create-falcon-app/src/index.js
@@ -1,18 +1,7 @@
-const { resolve } = require('path');
+const { dirname, resolve } = require('path');
 const fs = require('fs-extra');
-const ora = require('ora');
-const { eraseLine } = require('ansi-escapes');
-
-const getSpinner = msg => {
-  const spinner = ora(msg);
-  spinner.color = 'blue';
-  spinner.start();
-
-  return () => {
-    spinner.stop();
-    process.stdout.write(eraseLine);
-  };
-};
+const execa = require('execa');
+const Listr = require('listr');
 
 const examplesPath = resolve(__dirname, './../examples');
 
@@ -30,14 +19,31 @@ const copyFolder = (source, dest) => {
   });
 };
 
-const copyTemplate = (templatePath, targetPath) => {
-  const stopSinner = getSpinner(`Copying template files to ${targetPath} ...`);
-  fs.copySync(templatePath, targetPath);
-  stopSinner();
+const getPackageManager = () => {
+  try {
+    execa.sync('yarnpkg', '--version');
+    return 'yarn';
+  } catch (e) {
+    return 'npm';
+  }
+};
+
+const getActiveProjects = targetPath => {
+  const folders = [];
+
+  if (fs.existsSync(resolve(targetPath, 'package.json'))) {
+    folders.push(targetPath);
+  } else {
+    folders.push(...fs.readdirSync(targetPath).map(folder => resolve(targetPath, folder)));
+  }
+
+  return folders;
 };
 
 const createFalconApp = ({ name, example }) => {
   const targetPath = resolve(process.cwd(), name);
+  const baseProjectPath = `${dirname(targetPath)}/`;
+
   const templatePath = resolve(examplesPath, example || 'shop-with-blog');
 
   if (!fs.existsSync(templatePath)) {
@@ -47,7 +53,31 @@ const createFalconApp = ({ name, example }) => {
     throw new Error(`"${targetPath}" exists`);
   }
 
-  copyTemplate(templatePath, targetPath);
+  const tasks = new Listr([
+    {
+      title: `Generating base structure for ${name} App`,
+      task: () => fs.copySync(templatePath, targetPath)
+    },
+    {
+      title: 'Installing dependencies',
+      task: ctx => {
+        const subTasks = [];
+        ctx.packageManager = getPackageManager();
+        ctx.activeProjects = getActiveProjects(targetPath);
+
+        ctx.activeProjects.forEach(folder => {
+          subTasks.push({
+            title: `for ${folder.replace(baseProjectPath, '')}`,
+            task: () => execa(ctx.packageManager, ['install'], { cwd: folder })
+          });
+        });
+
+        return new Listr(subTasks, { concurrent: true });
+      }
+    }
+  ]);
+
+  return tasks.run();
 };
 
 module.exports = createFalconApp;
