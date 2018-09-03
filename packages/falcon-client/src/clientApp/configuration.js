d899dc2d2d392cbb939f0cb40b741ed92d7dbf23
tutok <walas.marcin@gmail.com>
2018-09-03 11:20:50 +0200

    feat(locales): using deeepmerge to merge default config with user customisation

--- packages/falcon-client/src/clientApp/configuration.js
+++ packages/falcon-client/src/clientApp/configuration.js
@@ -1,25 +1,28 @@
 import configuration from 'app-path/falcon-client.config.js';
+import deepMerge from 'deepmerge';
 
-const { logLevel = 'error', serverSideRendering = true, usePwaManifest = true, googleTagManager = { id: null } } =
-  configuration.config || {};
-
-const config = {
-  __typename: 'Config',
-  logLevel,
-  serverSideRendering,
-  usePwaManifest,
-  googleTagManager: {
-    __typename: 'GoogleTagManager',
-    ...googleTagManager
+const config = deepMerge(
+  {
+    __typename: 'Config',
+    logLevel: 'error',
+    serverSideRendering: true,
+    usePwaManifest: true,
+    googleTagManager: {
+      __typename: 'GoogleTagManager',
+      id: null
+    },
+    i18n: {
+      __typename: 'I18n',
+      lng: 'en',
+      ns: ['common'],
+      fallbackLng: 'en',
+      whitelist: ['en']
+      // available: langs (should be fetch from falcon-server)
+    }
   },
-  language: {
-    __typename: 'Language',
-    default: 'en'
-    // all: ['en', 'nl', 'pl', 'de', 'fr']
-    // namespaces: ["common", "blog", "shop", "account"]
-    // available: langs (should be fetch from falcon-server)
-  }
-};
+  configuration.config,
+  { arrayMerge: (destination, source) => source }
+);
 
 export default {
   config,
