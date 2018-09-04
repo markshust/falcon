46ea19d4f7d2664c3e734e7049c60263afde90a5
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-09-04 16:00:37 +0200

    feat(falcon-ui): add portal component

---
+++ packages/falcon-ui/src/components/Portal.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Box } from './';
+
+export class Portal extends React.Component {
+  state = { wrapper: null };
+
+  componentDidMount() {
+    const wrapper = document.createElement('div');
+    document.body.appendChild(wrapper);
+    // eslint-disable-next-line
+    this.setState({ wrapper });
+  }
+
+  componentWillUnmount() {
+    const { wrapper } = this.state;
+    if (wrapper) {
+      document.body.removeChild(wrapper);
+    }
+  }
+
+  render() {
+    const { wrapper } = this.state;
+    if (wrapper) {
+      return ReactDOM.createPortal(<Box {...this.props} />, wrapper);
+    }
+
+    return null;
+  }
+}
