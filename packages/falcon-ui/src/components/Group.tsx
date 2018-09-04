9e05971195705048a32a629e10d9d94245ba7dce
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-09-04 16:04:49 +0200

    feat(falcon-ui): add Group component

---
+++ packages/falcon-ui/src/components/Group.tsx
@@ -0,0 +1,29 @@
+import { themed } from '../theme';
+
+export const Group = themed(
+  {
+    themeKey: 'group',
+    tag: 'div'
+  },
+  {
+    display: 'flex',
+    css: {
+      // use first of type not first-child
+      // details: https://github.com/emotion-js/emotion/issues/637
+      '& > :first-of-type': {
+        borderTopRightRadius: 0,
+        borderBottomRightRadius: 0,
+        boxShadow: 'none'
+      },
+      '> :not(:first-of-type):not(:last-child)': {
+        borderRadius: 0,
+        boxShadow: 'none'
+      },
+      '> :last-child': {
+        borderTopLeftRadius: 0,
+        borderBottomLeftRadius: 0,
+        boxShadow: 'none'
+      }
+    }
+  }
+);
