48e11cf60de077edde3f7c80f507171f9fd8d356
tutok <walas.marcin@gmail.com>
2018-09-05 14:14:10 +0200

    feat(locales): fixed loading resources on server * needs to use sync-fs-backend * when development all resources will be reloded on each request, when production it will be cached as default behaviour of koa-i18next middleware

--- packages/falcon-client/src/i18n/i18nextMiddleware.js
+++ packages/falcon-client/src/i18n/i18nextMiddleware.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import i18next from 'i18next';
-import Backend from 'i18next-node-fs-backend';
+import Backend from 'i18next-sync-fs-backend';
 import koaI18next from 'koa-i18next';
 
 const i18nextServer = ({ lng = 'en', ns = ['common'], fallbackLng = 'en', whitelist = ['en'] } = {}) =>
@@ -11,6 +11,7 @@ const i18nextServer = ({ lng = 'en', ns = ['common'], fallbackLng = 'en', whitel
     whitelist,
     defaultNS: 'common',
     fallbackNS: 'common',
+    initImmediate: false,
     react: {
       nsMode: 'fallback'
     },
@@ -23,12 +24,23 @@ const i18nextServer = ({ lng = 'en', ns = ['common'], fallbackLng = 'en', whitel
     }
   });
 
-export default options =>
-  koaI18next(i18nextServer(options), {
-    lookupCookie: 'i18n',
-    order: ['cookie'],
-    next: true // if koa is version 2
-  });
+export default options => {
+  // TODO load available languages from falcon-server using apollo client
+  // TODO this means that we need to create SINGLE instance of Apollo Client and inject it into middleware instead of creating it each time!
+  const i18nextInstance = i18nextServer(options);
+
+  return async (ctx, next) => {
+    if (process.env.NODE_ENV === 'development') {
+      i18nextInstance.reloadResources();
+    }
+
+    return koaI18next(i18nextInstance, {
+      lookupCookie: 'i18n',
+      order: ['cookie'],
+      next: true // if koa is version 2
+    })(ctx, next);
+  };
+};
 
 export function filterResourceStoreByNs(storeData, namespaces) {
   const i18nResourceStore = {};
