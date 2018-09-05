992c7383f3f5f97e207b503548fad3ef773f9ac1
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-09-05 14:54:04 +0200

    docs(falcon-ui): add script command to build docs, modify docz config to use our custom tsconfig, auto-generate netlify redirects file

--- packages/falcon-ui/doczrc.js
+++ packages/falcon-ui/doczrc.js
@@ -1,7 +1,32 @@
+// eslint-disable-next-line
+import doczPluginNetlify from 'docz-plugin-netlify';
+
 module.exports = {
   typescript: true,
   src: './',
   wrapper: 'docs/Wrapper',
   title: 'Falcon UI',
-  propsParser: false
+  themeConfig: {
+    colors: {
+      background: '#fafafa'
+    },
+    styles: {
+      playground: {
+        background: '#fff'
+      }
+    }
+  },
+  modifyBundlerConfig: config => {
+    const jsxPluginIndex = config.plugins.findIndex(plugin => plugin.config.id === 'jsx');
+    const { loaders } = config.plugins[jsxPluginIndex].config;
+    const docGenLoaderIndex = loaders.findIndex(loader => /react-docgen-typescript-loader/.test(loader.loader));
+    const docGenLoader = loaders[docGenLoaderIndex];
+
+    docGenLoader.options = {
+      tsconfigPath: './tsconfig.json'
+    };
+
+    return config;
+  },
+  plugins: [doczPluginNetlify()]
 };
