16eb6fdf3a475fe5d38da48fb74040fb807ff12d
tutok <walas.marcin@gmail.com>
2018-09-14 15:17:45 +0200

    feat(app-shell): added isonline HOC and used it on app

--- examples/shop-with-blog/client/src/App.js
+++ examples/shop-with-blog/client/src/App.js
@@ -1,11 +1,12 @@
 import { asyncComponent } from 'react-async-component';
 import React from 'react';
+import PropTypes from 'prop-types';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
 import Home from 'src/pages/home/Home';
 import LogIn from 'src/pages/login/LogIn';
 import DynamicRoute from '@deity/falcon-client/src/components/DynamicRoute';
-import OfflineOverlay from '@deity/falcon-client/src/components/OfflineOverlay';
+import isOnline from '@deity/falcon-client/src/components/isOnline';
 import 'src/App.css';
 
 const components = {
@@ -17,14 +18,21 @@ const components = {
   })
 };
 
-const App = () => (
-  <OfflineOverlay>
+const App = ({ online }) => (
+  <React.Fragment>
+    {!online && <p>your are offline.</p>}
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={LogIn} />
       <DynamicRoute components={components} />
     </Switch>
-  </OfflineOverlay>
+  </React.Fragment>
 );
 
-export default App;
+App.propTypes = {
+  online: PropTypes.bool
+  // t: PropTypes.func,
+  // children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
+};
+
+export default isOnline()(App);
