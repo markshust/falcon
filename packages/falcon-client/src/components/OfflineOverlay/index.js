9324d9a6a4bc2eef2714ea777bb2de24e5d5af23
tutok <walas.marcin@gmail.com>
2018-09-16 11:23:00 +0200

    feat(app-shell): added isOnline HOC

--- packages/falcon-client/src/components/OfflineOverlay/index.js
+++ packages/falcon-client/src/components/OfflineOverlay/index.js
@@ -1,60 +1,108 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { translate } from 'react-i18next';
 // import './styles.scss';
 
-class OfflineOverlay extends Component {
-  constructor() {
-    super();
-    this.state = {
-      online: true
-    };
+const isOnline = () => WrappedComponent => {
+  class HOC extends Component {
+    constructor(props, context) {
+      super(props, context);
 
-    this.updateOnLineStatus = this.updateOnLineStatus.bind(this);
-  }
+      this.state = {
+        online: true
+      };
 
-  componentDidMount() {
-    window.addEventListener('online', this.updateOnLineStatus);
-    window.addEventListener('offline', this.updateOnLineStatus);
-
-    // run detection logic when component mounts as React 16 optimizes too much and doesn't
-    // re-render even when SSR html doesn't match client-side
-    // more  details: https://github.com/facebook/react/issues/10591
-    this.updateOnLineStatus();
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('online', this.updateOnLineStatus);
-    window.removeEventListener('offline', this.updateOnLineStatus);
-  }
-
-  updateOnLineStatus() {
-    const isOnline = navigator && navigator.onLine;
-    if (this.state.online === isOnline) {
-      return;
+      this.updateOnLineStatus = this.updateOnLineStatus.bind(this);
     }
 
-    this.setState({
-      online: isOnline
-    });
+    componentDidMount() {
+      window.addEventListener('online', this.updateOnLineStatus);
+      window.addEventListener('offline', this.updateOnLineStatus);
+
+      // run detection logic when component mounts as React 16 optimizes too much and doesn't
+      // re-render even when SSR html doesn't match client-side
+      // more  details: https://github.com/facebook/react/issues/10591
+      this.updateOnLineStatus();
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener('online', this.updateOnLineStatus);
+      window.removeEventListener('offline', this.updateOnLineStatus);
+    }
+
+    updateOnLineStatus() {
+      const online = navigator && navigator.onLine;
+      if (this.state.online === online) {
+        return;
+      }
+
+      this.setState({
+        online
+      });
+    }
+
+    render() {
+      const { online } = this.state;
+
+      return <WrappedComponent online={online} {...this.props} />;
+    }
   }
 
-  render() {
-    const { t, children } = this.props;
-    const { online } = this.state;
-
-    return (
-      <div className={online ? 'online' : 'offline'}>
-        {!online && <div className="message">{t('common.offline')}</div>}
-        {children}
-      </div>
-    );
-  }
-}
-
-OfflineOverlay.propTypes = {
-  t: PropTypes.func,
-  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
+  return HOC;
 };
 
-export default translate('common')(OfflineOverlay);
+export default isOnline;
+
+// class OfflineOverlay extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       online: true
+//     };
+
+//     this.updateOnLineStatus = this.updateOnLineStatus.bind(this);
+//   }
+
+//   componentDidMount() {
+//     window.addEventListener('online', this.updateOnLineStatus);
+//     window.addEventListener('offline', this.updateOnLineStatus);
+
+//     // run detection logic when component mounts as React 16 optimizes too much and doesn't
+//     // re-render even when SSR html doesn't match client-side
+//     // more  details: https://github.com/facebook/react/issues/10591
+//     this.updateOnLineStatus();
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('online', this.updateOnLineStatus);
+//     window.removeEventListener('offline', this.updateOnLineStatus);
+//   }
+
+//   updateOnLineStatus() {
+//     const isOnline = navigator && navigator.onLine;
+//     if (this.state.online === isOnline) {
+//       return;
+//     }
+
+//     this.setState({
+//       online: isOnline
+//     });
+//   }
+
+//   render() {
+//     const { t, children } = this.props;
+//     const { online } = this.state;
+
+//     return (
+//       <div className={online ? 'online' : 'offline'}>
+//         {!online && <div className="message">{t('common.offline')}</div>}
+//         {children}
+//       </div>
+//     );
+//   }
+// }
+
+// OfflineOverlay.propTypes = {
+//   t: PropTypes.func,
+//   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node])
+// };
+
+// export default translate('common')(OfflineOverlay);
