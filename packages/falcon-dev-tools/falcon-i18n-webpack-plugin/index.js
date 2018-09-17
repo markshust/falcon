b415e83d3c5f9671004a395b113582779a62abc4
tutok <walas.marcin@gmail.com>
2018-09-17 10:12:46 +0200

    feat(app-shell): add hmr support into i18n falcon plugin

--- packages/falcon-dev-tools/falcon-i18n-webpack-plugin/index.js
+++ packages/falcon-dev-tools/falcon-i18n-webpack-plugin/index.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const fs = require('fs-extra');
 const merge = require('deepmerge');
+const getI18nResourceFileDefinitions = require('./src');
 
 module.exports = class FalconI18nPlugin {
   /**
    * Webpack Plugin which merges localization json files from specified directories and emit them into configurable directory.
    * Files emitting is done on compilation hook, which allows to import them and build chunks.
-   * @param {{mainSource, defaultSources, output }} options - options
+   * @param {{ mainSource, defaultSources, output }} options - options
    */
   constructor({ mainSource, defaultSources = [], output = 'public/i18n', filter }) {
     this.name = 'FalconI18nPlugin';
@@ -24,104 +25,59 @@ module.exports = class FalconI18nPlugin {
         ns: filter && Array.isArray(filter.ns) ? filter.ns : []
       }
     };
+
+    this.resourceFileDefinitions = getI18nResourceFileDefinitions(mainSource, defaultSources, filter);
   }
 
   apply(compiler) {
-    const { mainSource, defaultSources, output, filter } = this.options;
-
-    this.localeFileDefinitions = this.getlocaleFileDefinitions(mainSource, defaultSources, filter);
+    const { output } = this.options;
 
     compiler.hooks.entryOption.tap(this.name, () => {
       fs.emptyDirSync(path.join(compiler.context, output));
+      this.resourceFileDefinitions.forEach(x => this.createOutputResourceFile(x, output, compiler.context));
     });
-    compiler.hooks.compilation.tap(this.name, () =>
-      this.openMergeAndWrite(this.localeFileDefinitions, output, compiler.context)
-    );
+
+    compiler.hooks.watchRun.tapAsync(this.name, (_compiler, done) => {
+      this.getChangedFiles(_compiler)
+        .reduce((result, file) => {
+          const changedDef = this.resourceFileDefinitions.find(def => def.sources.some(x => x === file));
+
+          return changedDef ? [...result, changedDef] : result;
+        }, [])
+        .forEach(x => this.createOutputResourceFile(x, output, _compiler.context));
+
+      return done();
+    });
+
     compiler.hooks.afterEmit.tap(this.name, compilation => this.watchSourceLocaleFiles(compilation));
   }
 
-  /**
-   * Get localizations file paths from specified directory.
-   * @param {string} directory - directories to search for locales.
-   * @param {string[]} lngFilter - languages which should be returned, if empty no filtration.
-   * @param {string[]} nsFilter - namespaces which should be returned, if empty no filtration.
-   * @returns {Object.<string, Object.<string, string[]>>} - language dictionary of namespace dictionary of file paths array.
-   */
-  getLocalizationFilePaths(directory, lngFilter = [], nsFilter = []) {
-    return fs
-      .readdirSync(directory)
-      .filter(lng => fs.lstatSync(path.join(directory, lng)).isDirectory())
-      .filter(lng => lngFilter.length === 0 || lngFilter.find(x => x === lng))
-      .reduce(
-        (languages, lng) =>
-          merge(languages, {
-            [lng]: fs
-              .readdirSync(path.join(directory, lng))
-              .filter(ns => path.extname(ns) === '.json')
-              .map(ns => path.basename(ns, '.json'))
-              .filter(ns => nsFilter.length === 0 || nsFilter.find(x => x === ns))
-              .reduce(
-                (namespaces, ns) => ({
-                  ...namespaces,
-                  [ns]: [path.join(directory, lng, `${ns}.json`)]
-                }),
-                {}
-              )
-          }),
-        {}
-      );
+  getChangedFiles(compiler) {
+    const { watchFileSystem } = compiler;
+    const watcher = watchFileSystem.watcher || watchFileSystem.wfs.watcher;
+
+    return Object.keys(watcher.mtimes);
   }
-
-  getlocaleFileDefinitions(mainSource, defaultSourceDirs, filter) {
-    const defaultI18nResources = defaultSourceDirs
-      .filter(x => fs.pathExistsSync(x))
-      .map(x => this.getLocalizationFilePaths(x, filter.lng, filter.ns))
-      .reduce((mergeResult, x) => merge(mergeResult, x), {});
-    const mainI18nResources = [mainSource].filter(x => fs.pathExistsSync(x)).map(x => this.getLocalizationFilePaths(x));
-    const merged = merge.all([defaultI18nResources, ...mainI18nResources]);
-
-    return this.flattenFilePathsDictionaries(merged);
-  }
-
-  /**
-   * Produce flat array of {lng, ns, sources}
-   * @param {Object.<string, Object.<string, string[]>>} localizations - languages dictionary of namespaces dictionary of file paths array.
-   * @returns {{lng: string, ns: string, sources: string[]}[]} - array of file paths sources for lng and ns.
-   */
-  flattenFilePathsDictionaries(localizations) {
-    const result = [];
-    Object.keys(localizations).forEach(lng => {
-      Object.keys(localizations[lng]).forEach(ns => {
-        result.push({
-          lng,
-          ns,
-          sources: localizations[lng][ns]
-        });
-      });
-    });
-
-    return result;
-  }
-
   /**
    * Opens all JSON files from definition, merges and writes
-   * @param {{lng: string, ns: string, sources: string[]}[]} sourceDefinitions - array of definitions according to which merging should be performed.
+   * @param {{lng: string, ns: string, sources: string[]}} sourceDefinition - definition according to which merging should be performed.
    * @param {string} output - directory where localization files should be stored
    * @param {string} relativeFrom - context path (required to ensure that output dir is created)
    * @returns {void}
    */
-  openMergeAndWrite(sourceDefinitions, output, relativeFrom) {
-    sourceDefinitions.forEach(({ lng, ns, sources }) => {
-      const outputFileRelativePath = path.relative(relativeFrom, `${path.join(output, lng, ns)}.json`);
-      const outputFileFullName = path.join(relativeFrom, outputFileRelativePath);
-      fs.ensureDirSync(path.dirname(outputFileFullName));
+  createOutputResourceFile(sourceDefinition, output, relativeFrom) {
+    const { lng, ns, sources } = sourceDefinition;
 
-      const mergedContent = sources
-        .map(x => path.relative(relativeFrom, x))
-        .map(x => fs.readJsonSync(x, 'utf8'))
-        .reduce((result, x) => merge(result, x), {});
-      fs.writeJsonSync(outputFileFullName, mergedContent, { spaces: 2 });
-    });
+    const outputFileName = path.join(output, lng, `${ns}.json`);
+    const outputFileRelativePath = path.relative(relativeFrom, outputFileName);
+    const outputFileFullName = path.join(relativeFrom, outputFileRelativePath);
+    fs.ensureDirSync(path.dirname(outputFileFullName));
+
+    const mergedContent = sources
+      .map(x => path.relative(relativeFrom, x))
+      .map(x => fs.readJsonSync(x, 'utf8'))
+      .reduce((result, x) => merge(result, x), {});
+    fs.writeJsonSync(outputFileFullName, mergedContent, { spaces: 2 });
   }
 
   /**
@@ -130,7 +86,7 @@ module.exports = class FalconI18nPlugin {
    * @returns {void}
    */
   watchSourceLocaleFiles(compilation) {
-    this.localeFileDefinitions.forEach(definition =>
+    this.resourceFileDefinitions.forEach(definition =>
       definition.sources.forEach(file => {
         compilation.fileDependencies.add(file);
       })
