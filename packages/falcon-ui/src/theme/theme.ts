12ef9e95682b835d3f5d2d361ba67acde3f8467c
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-09-17 13:50:28 +0200

    improvement(falcon-ui): update default theme box shadow

--- packages/falcon-ui/src/theme/theme.ts
+++ packages/falcon-ui/src/theme/theme.ts
@@ -83,7 +83,7 @@ export const theme = {
 
   boxShadows: {
     none: 'none',
-    xs: '0 0 2px 0 rgba(0,0,0,.08),0 1px 4px 0 rgba(0,0,0,.16)',
+    xs: '0 5px 5px rgba(0,0,0,.1)',
     sm: '0 0 2px 0 rgba(0,0,0,.08),0 2px 8px 0 rgba(0,0,0,.16)',
     md: '0 0 2px 0 rgba(0,0,0,.08),0 4px 16px 0 rgba(0,0,0,.16)',
     lg: '0 0 2px 0 rgba(0,0,0,.08),0 8px 32px 0 rgba(0,0,0,.16)'
