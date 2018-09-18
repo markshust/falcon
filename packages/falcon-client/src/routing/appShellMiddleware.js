448bfd1bbf6f1031d60794bd69224f72d7a1db6d
tutok <walas.marcin@gmail.com>
2018-09-18 08:55:16 +0200

    feat(app-shell): moved requireing webpack Assets into middleware factory

--- packages/falcon-client/src/routing/appShellMiddleware.js
+++ packages/falcon-client/src/routing/appShellMiddleware.js
@@ -4,33 +4,35 @@ import { extractI18nextState } from '../i18n/i18nServerFactory';
 import Html from '../components/Html';
 import { APP_INIT } from '../graphql/config.gql';
 
-// eslint-disable-next-line
-const assets = process.env.RAZZLE_ASSETS_MANIFEST && require(process.env.RAZZLE_ASSETS_MANIFEST);
-
 /**
  * Application shell renderer middleware.
  * @return {function(ctx: object, next: function): Promise<void>} Koa middleware
  */
-export default () => async ctx => {
-  const { client, App, asyncContext, serverTiming } = ctx.state;
-  const { config } = client.readQuery({ query: APP_INIT });
+export default () => {
+  // eslint-disable-next-line
+  const webpackAssets = process.env.RAZZLE_ASSETS_MANIFEST && require(process.env.RAZZLE_ASSETS_MANIFEST);
 
-  const renderTimer = serverTiming.start('HTML renderToString()');
+  return async ctx => {
+    const { client, App, asyncContext, serverTiming } = ctx.state;
+    const { config } = client.readQuery({ query: APP_INIT });
 
-  const htmlDocument = renderToString(
-    <Html
-      assets={assets}
-      asyncContext={asyncContext}
-      state={client.extract()}
-      i18nextState={extractI18nextState(ctx)}
-      config={config}
-    >
-      {App}
-    </Html>
-  );
+    const renderTimer = serverTiming.start('HTML renderToString()');
 
-  serverTiming.stop(renderTimer);
+    const htmlDocument = renderToString(
+      <Html
+        assets={webpackAssets}
+        asyncContext={asyncContext}
+        state={client.extract()}
+        i18nextState={extractI18nextState(ctx)}
+        config={config}
+      >
+        {App}
+      </Html>
+    );
 
-  ctx.status = 200;
-  ctx.body = `<!doctype html>${htmlDocument}`;
+    serverTiming.stop(renderTimer);
+
+    ctx.status = 200;
+    ctx.body = `<!doctype html>${htmlDocument}`;
+  };
 };
