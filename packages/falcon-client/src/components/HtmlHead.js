e84bb1800123e18fe27e934b2ca1ca67aa228f98
tutok <walas.marcin@gmail.com>
2018-09-19 11:12:48 +0200

    feat(app-shell): as we have some predefined HtmlHead, added favicon to it

--- packages/falcon-client/src/components/HtmlHead.js
+++ packages/falcon-client/src/components/HtmlHead.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import favicon from 'app-path/src/assets/favicon.ico';
 
 export default function HtmlHead({ htmlLang }) {
   return (
     <Helmet htmlAttributes={{ lang: htmlLang }}>
+      <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="shortcut icon" href={favicon} />
     </Helmet>
   );
 }
