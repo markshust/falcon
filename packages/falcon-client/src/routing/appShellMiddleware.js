b881f2c4a9a4f3a47f256f4ad4bf5f6c68b8e8cf
tutok <walas.marcin@gmail.com>
2018-09-19 09:52:58 +0200

    feat(app-shell): as extracting webpack assets were moved into separate middleware then appshell middleware gets simplified

--- packages/falcon-client/src/routing/appShellMiddleware.js
+++ packages/falcon-client/src/routing/appShellMiddleware.js
@@ -2,45 +2,29 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { extractI18nextState } from '../i18n/i18nServerFactory';
 import Html from '../components/Html';
-import { APP_INIT } from '../graphql/config.gql';
 
 /**
  * Application shell renderer middleware.
  * @return {function(ctx: object, next: function): Promise<void>} Koa middleware
  */
-export default () => {
-  // eslint-disable-next-line
-  let assetsManifest = process.env.RAZZLE_ASSETS_MANIFEST && require(process.env.RAZZLE_ASSETS_MANIFEST);
+export default () => async ctx => {
+  const { App, client, asyncContext, helmetContext, serverTiming, webpackAssets } = ctx.state;
+  const renderTimer = serverTiming.start('HTML renderToString()');
 
-  const assets = {
-    clientJs: assetsManifest.client.js,
-    clientCss: assetsManifest.client.css,
-    vendorsJs: assetsManifest.vendors.js,
-    webmanifest: assetsManifest[''] && assetsManifest[''].webmanifest
-  };
+  const htmlDocument = renderToString(
+    <Html
+      assets={webpackAssets}
+      asyncContext={asyncContext}
+      helmetContext={helmetContext}
+      state={client.extract()}
+      i18nextState={extractI18nextState(ctx)}
+    >
+      {App}
+    </Html>
+  );
 
-  return async ctx => {
-    const { client, App, asyncContext, helmetContext, serverTiming } = ctx.state;
-    const { config } = client.readQuery({ query: APP_INIT });
+  serverTiming.stop(renderTimer);
 
-    const renderTimer = serverTiming.start('HTML renderToString()');
-
-    const htmlDocument = renderToString(
-      <Html
-        assets={assets}
-        asyncContext={asyncContext}
-        helmetContext={helmetContext}
-        state={client.extract()}
-        i18nextState={extractI18nextState(ctx)}
-        config={config}
-      >
-        {App}
-      </Html>
-    );
-
-    serverTiming.stop(renderTimer);
-
-    ctx.status = 200;
-    ctx.body = `<!doctype html>${htmlDocument}`;
-  };
+  ctx.status = 200;
+  ctx.body = `<!doctype html>${htmlDocument}`;
 };
