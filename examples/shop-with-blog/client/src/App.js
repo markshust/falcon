6b106cb73964760489ce4f306d45c59d9bcdf1f2
tutok <walas.marcin@gmail.com>
2018-09-19 11:13:57 +0200

    feat(app-shell): added developer custom HeadMetaTags component

--- examples/shop-with-blog/client/src/App.js
+++ examples/shop-with-blog/client/src/App.js
@@ -5,9 +5,27 @@ import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
 import Home from 'src/pages/home/Home';
 import LogIn from 'src/pages/login/LogIn';
+import Helmet from 'react-helmet';
 import DynamicRoute from '@deity/falcon-client/src/components/DynamicRoute';
 import isOnline from '@deity/falcon-client/src/components/isOnline';
 import 'src/App.css';
+import logo from 'src/assets/logo.png';
+
+const HeadMetaTags = () => (
+  <Helmet defaultTitle="Deity Shop with Blog" titleTemplate="%s | Deity Shop with Blog">
+    <meta name="description" content="This is example of Shop with Blog powered by Deity Falcon" />
+    <meta name="keywords" content="pwa,reactjs,ecommerce,magento,shop,webshop,deity" />
+    <meta name="theme-color" content="#fff" />
+    <meta name="format-detection" content="telephone=yes" />
+    <meta property="og:title" content="Deity Shop with Blog" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="This is example of Shop with Blog powered by Deity Falcon" />
+    <meta property="og:image" content={logo} />
+    <meta property="og:url" content="/" />
+    <meta property="og:image:width" content="300" />
+    <meta property="og:image:height" content="107" />
+  </Helmet>
+);
 
 const components = {
   shop: asyncComponent({
@@ -20,6 +38,7 @@ const components = {
 
 const App = ({ online }) => (
   <React.Fragment>
+    <HeadMetaTags />
     {!online && <p>your are offline.</p>}
     <Switch>
       <Route exact path="/" component={Home} />
