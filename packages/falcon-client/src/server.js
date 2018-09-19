dab35be480195daa2648a619bbbaafb11f6ba07d
tutok <walas.marcin@gmail.com>
2018-09-19 15:43:11 +0200

    feat(app-shell): ok, koa-static expect ms instaed of s, set cache to 1 year!

--- packages/falcon-client/src/server.js
+++ packages/falcon-client/src/server.js
@@ -26,7 +26,7 @@ export default ({ App, clientApolloSchema, configuration }) => {
 
   const staticFiles = new Router();
   staticFiles.get('/sw.js', serve(process.env.RAZZLE_PUBLIC_DIR, { maxage: 0 }));
-  staticFiles.get('/static/*', serve(process.env.RAZZLE_PUBLIC_DIR, { maxage: 60 * 60 * 24 /* 1d */ }));
+  staticFiles.get('/static/*', serve(process.env.RAZZLE_PUBLIC_DIR, { maxage: 31536000 * 1000 /* 1y */ }));
 
   const router = new Router();
   router.get('/app-shell', ...renderAppShell({ configuration }));
