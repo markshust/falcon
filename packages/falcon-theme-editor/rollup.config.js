97eedf02235816945a53680633e409a2de768345
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-09-23 21:14:28 +0200

    feat: add falcon-theme-editor package

---
+++ packages/falcon-theme-editor/rollup.config.js
@@ -0,0 +1,32 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import pkg from './package.json';
+
+process.env.ROLLUP = true;
+
+const externals = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
+
+const makeExternalPredicate = externalsArr => {
+  if (externalsArr.length === 0) {
+    return () => false;
+  }
+  const externalPattern = new RegExp(`^(${externalsArr.join('|')})($|/)`);
+  return id => externalPattern.test(id);
+};
+
+const extensions = ['.ts', '.tsx', '.js', '.jsx'];
+
+export default {
+  input: 'src/index.tsx',
+  external: makeExternalPredicate(externals),
+  plugins: [
+    resolve({
+      extensions
+    }),
+    babel({
+      extensions,
+      runtimeHelpers: true
+    })
+  ],
+  output: [{ file: pkg.main, format: 'cjs', sourcemap: true }]
+};
