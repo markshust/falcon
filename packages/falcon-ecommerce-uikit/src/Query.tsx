222265690fe95b5bb07dc1be5ae2f8366af7db1d
Tadeusz Wójcik <tadeuszwojcik@gmail.com>
2018-09-26 14:47:52 +0200

    feat(ecommerce-tools): show loader component when query is still loading data

--- packages/falcon-ecommerce-uikit/src/Query.tsx
+++ packages/falcon-ecommerce-uikit/src/Query.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
 import { Query as ApolloQuery } from 'react-apollo';
+import { Icon, themed } from '@deity/falcon-ui';
+
+export const LoaderLayout = themed({
+  tag: 'div',
+  defaultTheme: {
+    loaderLayout: {
+      height: '100vh',
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+  }
+});
 
 export const Query: React.SFC<{
   children: (data: any) => any;
@@ -7,7 +21,14 @@ export const Query: React.SFC<{
 }> = ({ children, query, ...rest }) => (
   <ApolloQuery query={query} {...rest}>
     {({ loading, error, data }) => {
-      if (loading) return null;
+      if (loading) {
+        return (
+          <LoaderLayout>
+            <Icon src="loader" />
+          </LoaderLayout>
+        );
+      }
+
       if (error) return `Error!: ${error}`;
 
       return children(data);
