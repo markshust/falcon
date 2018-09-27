d78c3d58e3e7e8b73972303f9f0bd4a84214acbf
tutok <walas.marcin@gmail.com>
2018-09-27 11:16:56 +0200

    feat(query-components): renderind Product page

--- examples/shop-with-blog/client/src/pages/shop/Product.js
+++ examples/shop-with-blog/client/src/pages/shop/Product.js
@@ -1,17 +1,5 @@
-import React from 'react';
+// import React from 'react';
 import { translate } from 'react-i18next';
-import PropTypes from 'prop-types';
-
-const Product = ({ id, path, type, t }) => (
-  <div>
-    <h1>{t('product.title')}</h1>
-    <p>{`type: ${type}`}</p>
-    <p>{`id: ${id}`}</p>
-    <p>{`path: ${path}`}</p>
-  </div>
-);
-Product.propTypes = {
-  t: PropTypes.func.isRequired
-};
+import { Product } from '@deity/falcon-ecommerce-uikit';
 
 export default translate('shop')(Product);
