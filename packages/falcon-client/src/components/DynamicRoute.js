cc34567059c1862af123ed13b6e26f5c0482947e
tutok <walas.marcin@gmail.com>
2018-10-01 15:17:44 +0200

    feat(query-components): simplified DynamicRoute

--- packages/falcon-client/src/components/DynamicRoute.js
+++ packages/falcon-client/src/components/DynamicRoute.js
@@ -2,52 +2,26 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
-import { asyncComponent } from 'react-async-component';
+import Query from './Query';
 
-// function isPromise(object) {
-//   if (Promise && Promise.resolve) {
-//     return Promise.resolve(object) === object;
-//   }
-//   throw new Error('Promise not supported in your environment');
-// }
+export const GET_URL = gql`
+  query URL($path: String!) {
+    url(path: $path) {
+      type
+      redirect
+      id
+      path
+    }
+  }
+`;
 
-const Loader = () => <p>Loading...</p>;
-const Error500 = ({ message }) => (
-  <div>
-    <p>Error!</p>
-    <p>{message}</p>
-  </div>
-);
-
-const DynamicRoute = props => {
-  const { components, location } = props;
+const DynamicRoute = ({ components, location, loaderComponent, errorComponent }) => {
   const { pathname } = location;
   const path = pathname.startsWith('/') ? pathname.substring(1) : pathname;
 
   return (
-    <Query
-      query={gql`
-        query URL($path: String!) {
-          url(path: $path) {
-            type
-            redirect
-            id
-            path
-          }
-        }
-      `}
-      variables={{ path }}
-    >
-      {({ loading, error, data }) => {
-        if (loading) {
-          return <Loader />;
-        }
-
-        if (error) {
-          return <Error500 message={error.message} />;
-        }
-
+    <Query query={GET_URL} variables={{ path }} loaderComponent={loaderComponent} errorComponent={errorComponent}>
+      {({ data }) => {
         if (!data || data.url === null) {
           return <p>not found</p>;
         }
@@ -62,27 +36,11 @@ const DynamicRoute = props => {
           return null;
         }
 
-        const componentProps = {
-          id: url.id,
-          path: url.path
-        };
-        // if (isPromise(component)) {
-        return React.createElement(
-          asyncComponent({
-            resolve: component,
-            LoadingComponent: Loader,
-            ErrorComponent: Error500
-          }),
-          { ...componentProps }
-        );
-        // }
-
-        // return React.createElement(component, { ...componentProps });
+        return React.createElement(component, { id: url.id, path: url.path });
       }}
     </Query>
   );
 };
-
 DynamicRoute.propTypes = {
   components: PropTypes.shape({}),
   location: PropTypes.shape({
@@ -92,57 +50,45 @@ DynamicRoute.propTypes = {
 
 export default DynamicRoute;
 
-// const DynamicSwitch = props => {
+// export const DynamicSwitch = props => {
 //   const { components, location } = props;
 //   const { pathname } = location;
 //   const path = pathname.startsWith('/') ? pathname.substring(1) : pathname;
 
 //   return (
-//     <Query
-//       query={gql`
-//         query URL($path: String!) {
-//           url(path: $path) {
-//             id
-//             path
-//             type
-//           }
-//         }
-//       `}
-//       variables={{ path }}
-//     >
+//     <Query query={GET_URL} variables={{ path }}>
 //       {({ loading, error, data }) => {
 //         if (loading) {
 //           return <Loader />;
 //         }
 
-//         if (error) {
-//           return <Error500 message={error.message} />;
-//         }
-
-//         return <Switch location={{ pathname: '' }} />;
-
 //         if (!data || data.url === null) {
-//           // return <Redirect to="/not-found" />;
+//           return <p>not found</p>;
 //         }
 
-//         const { type } = data.url;
-//         const component = components[type];
+//         const { url } = data;
+//         if (url.redirect) {
+//           return <Redirect to={`/${url.path}`} />;
+//         }
+
+//         const component = components[url.type];
 //         if (!component) {
 //           return null;
 //         }
 
-//         if (isPromise(component)) {
-//           return React.createElement(
-//             asyncComponent({
-//               resolve: () => component,
-//               LoadingComponent: Loader,
-//               ErrorComponent: Error500
-//             }),
-//             { ...data.url }
-//           );
-//         }
-
-//         return React.createElement(component, { ...data.url });
+//         return <Switch location={{ pathname: `${url.path}?id=${url.id}` }}>{props.children}</Switch>;
+//         // return React.createElement(
+//         //   asyncComponent({
+//         //     resolve: component,
+//         //     LoadingComponent: Loader,
+//         //     ErrorComponent: Error500
+//         //   }),
+//         //   {
+//         //     id: url.id,
+//         //     path: url.path
+//         //   }
+//         // );
+//         // }
 //       }}
 //     </Query>
 //   );
