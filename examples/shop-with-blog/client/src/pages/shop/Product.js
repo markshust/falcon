d4df67c273fb9675aaf7ef2f71d32bc9e34ba19a
tutok <walas.marcin@gmail.com>
2018-10-01 15:18:11 +0200

    feat(query-components):  clean up

--- examples/shop-with-blog/client/src/pages/shop/Product.js
+++ examples/shop-with-blog/client/src/pages/shop/Product.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { ProductQuery, Product } from '@deity/falcon-ecommerce-uikit';
 
 const ProductPage = ({ id }) => (
-  // <p>Id: {id}</p>
   <ProductQuery variables={{ id }}>{productProps => <Product {...productProps} />}</ProductQuery>
 );
 ProductPage.propTypes = {
