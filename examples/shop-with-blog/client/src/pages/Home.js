5d4193a0d265d7d5882599712e30bd79436a2c15
tutok <walas.marcin@gmail.com>
2018-10-02 10:33:07 +0200

    feat(query-components): added translations for home

--- examples/shop-with-blog/client/src/pages/Home.js
+++ examples/shop-with-blog/client/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { I18n } from 'react-i18next';
+
 import { H1, Box, FlexLayout } from '@deity/falcon-ui';
 import { ProductsList, ProductsListQuery } from '@deity/falcon-ecommerce-uikit';
 
 const Home = () => (
   <Box my="lg">
     <FlexLayout my="lg" justifyContent="center">
-      <H1>Hot sellers</H1>
+      <I18n ns={['shop']}>{t => <H1>{t('home.hotSellers')}</H1>}</I18n>
     </FlexLayout>
     <ProductsListQuery>{({ products }) => <ProductsList products={products.items} />}</ProductsListQuery>
   </Box>
